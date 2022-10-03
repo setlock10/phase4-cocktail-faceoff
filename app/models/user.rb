08f77dcaf609f5529a0b660fc24aeec1fae29198
John J Setlock II <johnsetlock@gmail.com>
2022-10-03 15:31:17 -0500

    login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true, uniqueness: tru:error
+    validates :username, presence: true, uniqueness: true
     
 end
