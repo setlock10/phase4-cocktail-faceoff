9289650befdf1e4a9ed9b6f9b1fddf03882bdaea
John J Setlock II <johnsetlock@gmail.com>
2022-10-03 16:20:59 -0500

    login added

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -4,12 +4,8 @@ class DrinksController < ApplicationController
         render json: Drink.find(params[:id]), status: :ok
     end
 
-    def random1
-        # byebug
-        render json: Drink.all.sample, status: :ok
-    end
     
-    def random2
+    def index
         two_drinks=[]
         drink1=Drink.all.sample
         two_drinks<<drink1
@@ -19,7 +15,7 @@ class DrinksController < ApplicationController
         while (drink1.id==drink2.id) do
             drink2=Drink.all.sample
         end
-        
+
         two_drinks<<drink2
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   resources :sessions
   resources :users
   resources :total_ratings, only:[:show, :update]
-  resources :drinks, only:[:show]
+  resources :drinks, only:[:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # get '/hello', to: 'application#hello_world'
 
-  get '/random1', to: 'drinks#random1'
-  get '/random2', to: 'drinks#random2'
+  # get '/random1', to: 'drinks#random1'
+  # get '/random2', to: 'drinks#random2'
   # post '/signup', to: 'users#create'
   # get '/me', to: 'users#show'
   # post '/login', to: 'sessions#create'
