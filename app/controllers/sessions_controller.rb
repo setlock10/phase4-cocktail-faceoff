3aec109d8c5f434d3cee73d8d1228e2ae9caa616
John J Setlock II <johnsetlock@gmail.com>
2022-10-03 15:17:17 -0500

    login added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only: :create
+    # skip_before_action :authorize, only: :create
 
-    def create
-        user =User.find_by(username: params[:username])
-        if user&.authenicate(params[:password]) 
-            session[:user_id] = user.id
-            render json: user, status: :ok
-        else
-            render json: {error:["Invalid password or username"]}, status: :unauthorized
-        end
-    end
+    # def create
+    #     user =User.find_by(username: params[:username])
+    #     if user&.authenicate(params[:password]) 
+    #         session[:user_id] = user.id
+    #         render json: user, status: :ok
+    #     else
+    #         render json: {error:["Invalid password or username"]}, status: :unauthorized
+    #     end
+    # end
 
 
-    def destroy
-        session.destroy :user_id
-        head :no_content
-    end
+    # def destroy
+    #     session.destroy :user_id
+    #     head :no_content
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,11 @@ Rails.application.routes.draw do
 
   get '/random1', to: 'drinks#random1'
   get '/random2', to: 'drinks#random2'
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  # post '/signup', to: 'users#create'
+  # get '/me', to: 'users#show'
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
 
-  get '*path',
-  to: 'fallback#index',
-  constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path',  to: 'fallback#index',  constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
